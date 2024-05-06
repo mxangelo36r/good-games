be5b182ca4ed5487a6426e5cb9aca9955b8594e1
Headley Brissett <hmbrsstt@memphis.edu>
2024-05-06 17:44:02 -0500

    added navbar and home page

--- client/good-games-ui/src/App.js
+++ client/good-games-ui/src/App.js
@@ -2,11 +2,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
   );
