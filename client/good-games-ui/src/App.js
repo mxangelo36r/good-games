2972ceda46c13f7cb16182493e85406c6a267963
Headley Brissett <hmbrsstt@memphis.edu>
2024-05-06 14:49:36 -0500

    created react project and installed react routes

---
+++ client/good-games-ui/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
