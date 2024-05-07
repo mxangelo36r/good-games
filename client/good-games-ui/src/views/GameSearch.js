0c798a5d8fb7fc06036283aeefc0f37fb7ee1efe
Headley Brissett <hmbrsstt@memphis.edu>
2024-05-07 17:22:12 -0500

    finished the first batch of work for the search feature

--- client/good-games-ui/src/views/GameSearch.js
+++ client/good-games-ui/src/views/GameSearch.js
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react";
-import { json, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const convert = require('xml-js');
 
-function GameSearch(props) {
+const convertToObj = (data) => {
+    const newGames = [];
+    data.elements[0].elements.map((e) => {
+        if (e.attributes.type === "boardgame") {
+            const newObj = {
+                bgg_id: e.attributes.id,
+                name: e.elements[0].attributes.value,
+                year_published: e.elements[1] ? e.elements[1].attributes.value : 0
+            }
+            newGames.push(newObj);
+        }
+    })
+    return newGames;
+}
+
+function GameSearch() {
     const [games, setGames] = useState([])
-    const [searchQuery, setSearchQuery] = useState("");
-    const [limit, setLimit] = useState(50);
     const [isLoading, setIsLoading] = useState(false);
 
     const bgg_url = 'https://api.geekdo.com/xmlapi2/search'
@@ -24,77 +37,40 @@ function GameSearch(props) {
             })
             .then(data => {
                 const obj = convert.xml2js(data);
-                console.log(obj.elements[0].elements[0].elements[0]);
-                console.log(obj)
+                setGames(convertToObj(obj));
             })
             .catch(console.log);
-
-            setSearchQuery(query);
         }
     }, [])
-    
-    const handleChange = (event) => {
-        setSearchQuery(event.target.value);
-    }
-
-    const handleSubmit = () => {
-        fetch(`${bgg_url}?search=${searchQuery}&exact=${limit}`)
-        .then(response => {
-            if (response.status === 200) {
-                console.log(response)
-                return response.body;
-            } else {
-                return Promise.reject(`Unexpected status code: ${response.status}`);
-            }
-        })
-        .then(data => setGames(data))
-        .catch(console.log);
-    }
 
     const renderRows = () => {
         return games.map((game, index) => (
             <tr key={index}>
                 <td></td>
+                <td>{game.bgg_id}</td>
+                <td>{game.name}</td>
                 <td></td>
-                <td></td>
-                <td></td>
+                <td>{game.year_published}</td>
             </tr>
         ))
     }
 
     return (
         <main className="container mt-4">
-            <header>
-                <form onSubmit={handleSubmit}>
-                    <div className="input-group">
-                        <input
-                        id="searchQuery"
-                        name="searchQuery"
-                        type="search"
-                        className="form-control"
-                        placeholder=""
-                        aria-label="Search"
-                        aria-describedby="btn-search"
-                        value={searchQuery}
-                        onChange={handleChange}
-                        />
-                        <button className="btn btn-outline-success" type="button" id="btn-search">Search</button>
-                    </div>
-                </form>
-            </header>
             <section>
                 <table className="table">
                     <caption>List of search results</caption>
                     <thead>
                         <tr>
                             <th scope="col"></th>
+                            <th scope="col">BGG ID</th>
                             <th scope="col">Game Name</th>
                             <th scope="col">Rating</th>
                             <th scope="col">Year Published</th>
                         </tr>
                     </thead>
                     <tbody className="table-group-divider">
-                        {/* {renderRows()} */}
+                        {renderRows()}
                     </tbody>
                 </table>
             </section>
