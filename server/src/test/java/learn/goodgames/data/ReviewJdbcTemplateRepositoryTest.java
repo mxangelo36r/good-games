a9a4c4a15ca7c19d4437a7b174915484fb4fdec9
angelomrosales <miguelrosalespersonal@gmail.com>
2024-05-07 20:30:03 -0500

    Implemented findAll() tests

---
+++ server/src/test/java/learn/goodgames/data/ReviewJdbcTemplateRepositoryTest.java
@@ -0,0 +1,34 @@
+package learn.goodgames.data;
+
+import learn.goodgames.models.Review;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class ReviewJdbcTemplateRepositoryTest {
+
+    @Autowired
+    ReviewJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+    @Test
+    void shouldFindAll() {
+        List<Review> all = repository.findAll();
+        assertNotNull(all);
+        assertEquals(7, all.size());
+    }
+    
+}
